91aaae668b0fdecfb92cc57de025d9b8d55f2921
r-ume <baymax.blackandwhite@gmail.com>
2017-11-26 16:44:07 +0900

    completed the view of instagram

--- InstagramApp/InstagramApp/ViewController.swift
+++ InstagramApp/InstagramApp/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var filterScrollView: UIScrollView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    filterScrollView.contentSize = CGSize(width: 730, height: 150)
+    
+    let button = UIButton()
+    button.frame = CGRect(x: 10, y: 10, width: 120, height: 120)
+    button.setBackgroundImage(UIImage(named: "cat.png"), for: UIControlState.normal)
+    button.layer.cornerRadius = 3.0
+    button.clipsToBounds = true
+//    button.addTarget(self, action: #selector(ViewController.tappedButton(_:)), for: .touchUpInside)
+    filterScrollView.addSubview(button)
+
+    
   }
 
   override func didReceiveMemoryWarning() {
