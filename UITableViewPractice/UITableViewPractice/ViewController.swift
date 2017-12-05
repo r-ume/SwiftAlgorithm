75463ff892350e8e800f07f90fb2807dfd1a9814
r-ume <baymax.blackandwhite@gmail.com>
2017-12-05 19:18:33 +0900

    initial commit of UITableViewPractice

---
+++ UITableViewPractice/UITableViewPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UITableViewPractice
+//
+//  Created by umeki on 2017/12/05.
+//  Copyright © 2017年 umeki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var myTableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
