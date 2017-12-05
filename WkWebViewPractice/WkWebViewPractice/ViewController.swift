7d5c618a4b603cf49f7d976ea19f8ac3334e418a
r-ume <baymax.blackandwhite@gmail.com>
2017-12-05 18:29:48 +0900

    added ActiionSheet & UIAlertController

--- WkWebViewPractice/WkWebViewPractice/ViewController.swift
+++ WkWebViewPractice/WkWebViewPractice/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 
 class ViewController: UIViewController, WKNavigationDelegate {
+  @IBOutlet weak var titleLabel: UILabel!
   
   let myWkWebView = WKWebView()
 
@@ -22,26 +23,32 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     view.addSubview(myWkWebView)
     myWkWebView.frame = CGRect(x: 0, y: 64, width: self.view.frame.width, height: self.view.frame.height - (64 + 44))
-    
-    let myURL = URL(string: "http://wired.jp/")
+
+    let myURL = URL(string: "https://github.com/")
     let myURLRequest = URLRequest(url: myURL!)
     myWkWebView.load(myURLRequest)
-  
+    
+    myWkWebView.addObserver(self, forKeyPath: "title", options: .new, context: nil)
 
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
+  func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutableRawPointer) {
+    //ヘッダーのLabelにWebページの記事のタイトルを表示
+    titleLabel.text = myWkWebView.title
+  }
+  
   //Webページの読み込みが開始したタイミングで、インジケータを表示
-  func webView(webView: WKWebView, didStartProvisionalNavigation : WKNavigation!) {
+  func webView(_: WKWebView, didStartProvisionalNavigation: WKNavigation!) {
     UIApplication.shared.isNetworkActivityIndicatorVisible = true
   }
   
   //Webページの読み込みが完了したタイミングで、インジケータを非表示
-  func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+  func webView(_: WKWebView, didFinish navigation: WKNavigation!) {
     UIApplication.shared.isNetworkActivityIndicatorVisible = false
   }
   
@@ -62,8 +69,15 @@ class ViewController: UIViewController, WKNavigationDelegate {
   }
   
   @IBAction func openActionSheet(_ sender: UIButton) {
+    let alertController = UIAlertController(title: "アクションシート", message: "アクションシートです。", preferredStyle: .actionSheet)
+    
+    self.present(alertController, animated: true, completion: nil)
   }
   
+  deinit {
+    //監視解除
+    myWkWebView.removeObserver(self, forKeyPath: "title")
+  }
   
   
 }
