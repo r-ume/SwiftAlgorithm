08da3743cafba350895760c0ea82c739da533a18
r-ume <baymax.blackandwhite@gmail.com>
2017-12-05 19:30:06 +0900

    defined numberOfRowsInSection, cellForRowAt, heightForRowAt, titleForHeaderInSection, heightForHeaderInSection delegate method

--- UITableViewPractice/UITableViewPractice/ViewController.swift
+++ UITableViewPractice/UITableViewPractice/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   @IBOutlet weak var myTableView: UITableView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    myTableView.delegate = self
+    myTableView.dataSource = self
   }
 
   override func didReceiveMemoryWarning() {
@@ -22,6 +24,33 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  //  行数10
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  //セルの内容
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "myCell")
+    cell.textLabel?.text = "Dog"
+    cell.detailTextLabel?.text = "This dog is a pug"
+    cell.imageView?.image = UIImage(named: "dog.png")
+    return cell
+  }
+  
+  //セルの高さ
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return CGFloat(60)
+  }
+  
+  //セクションのタイトル
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Animal"
+  }
+  
+  //セクションのタイトルの高さ
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return CGFloat(44)
+  }
 }
 
