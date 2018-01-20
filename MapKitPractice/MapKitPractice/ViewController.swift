f2f6dacf7abe67a6af5e8946675b787b0ae9bd8d
r-ume <baymax.blackandwhite@gmail.com>
2018-01-20 19:19:34 +0900

    able to make a pin on the designated area

--- MapKitPractice/MapKitPractice/ViewController.swift
+++ MapKitPractice/MapKitPractice/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  let myMapView = MKMapView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    myMapView.frame = self.view.frame
+    self.view.addSubview(myMapView)
+    
+    let longPressGesture = UILongPressGestureRecognizer()
+    longPressGesture.addTarget(self, action: #selector(longPressed(sender:)))
+    myMapView.addGestureRecognizer(longPressGesture)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @objc func longPressed(sender: UILongPressGestureRecognizer) {
+    if sender.state != UIGestureRecognizerState.changed {
+      return
+    }
+    
+    let tappedLocation = sender.location(in: myMapView)
+    let tappedPoint = myMapView.convert(tappedLocation, toCoordinateFrom: myMapView)
+    let annotation = MKPointAnnotation()
+    annotation.coordinate = tappedPoint
+    annotation.title = "タイトル"
+    annotation.subtitle = "サブタイトル"
+    self.myMapView.addAnnotation(annotation)
+    
+    print("Hello World!!")
+  }
 }
 
