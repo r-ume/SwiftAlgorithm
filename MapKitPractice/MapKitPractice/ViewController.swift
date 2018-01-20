6c4652332b7a1d47923b9192e23e3543ab6a2bec
r-ume <baymax.blackandwhite@gmail.com>
2018-01-20 14:47:10 +0900

    initial commit of MapKitPractice

---
+++ MapKitPractice/MapKitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapKitPractice
+//
+//  Created by umeki on 2018/01/20.
+//  Copyright © 2018年 umeki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
