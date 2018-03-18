640019d3d7adbfbb8a74b5c11921f0cb54fd1459
r-ume <baymax.blackandwhite@gmail.com>
2018-03-18 19:45:36 +0900

    added makeBackTweetView func

--- TwitterApp/TwitterApp/ViewController.swift
+++ TwitterApp/TwitterApp/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //-------------TableViewの処理------------------
 
+    
+    @IBAction func tapButton(_ sender: UIButton) {
+        let backTweetView = self.makeBackTweetView()
+        self.view.addSubview(backTweetView)
+    }
+    
+    //-------------部品の生成のための処理--------------
+    func makeBackTweetView() -> UIView{
+        let backTweetView = UIView()
+        backTweetView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        backTweetView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        return backTweetView
+    }
 }
 
