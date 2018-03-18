549618a80344c2aec8ae423f3c5486ada5d9486d
r-ume <baymax.blackandwhite@gmail.com>
2018-03-18 20:02:03 +0900

    added makeTweetView func

--- TwitterApp/TwitterApp/ViewController.swift
+++ TwitterApp/TwitterApp/ViewController.swift
@@ -26,6 +26,9 @@ class ViewController: UIViewController {
     @IBAction func tapButton(_ sender: UIButton) {
         let backTweetView = self.makeBackTweetView()
         self.view.addSubview(backTweetView)
+        
+        let tweetView = self.makeTweetView()
+        backTweetView.addSubview(tweetView)
     }
     
     //-------------部品の生成のための処理--------------
@@ -34,6 +37,17 @@ class ViewController: UIViewController {
         backTweetView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
         backTweetView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
         return backTweetView
+    }
+    
+    func makeTweetView() -> UIView{
+        let tweetView = UIView()
+        tweetView.frame.size = CGSize(width: 300, height: 300)
+        tweetView.center.x = self.view.center.x
+        tweetView.center.y = 250
+        tweetView.backgroundColor = UIColor.white
+        tweetView.layer.shadowOpacity = 0.3
+        tweetView.layer.cornerRadius = 3
+        return tweetView
     }
 }
 
