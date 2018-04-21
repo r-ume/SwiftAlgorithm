72a1b7936dd341b7fcb3f6a3705f1f33a6e118c0
romukey <baymax.blackandwhite@gmail.com>
2018-04-21 15:29:53 +0900

    changed the content in optional playground

--- Playground/Optional.playground/Contents.swift
+++ Playground/Optional.playground/Contents.swift
@@ -4,14 +4,11 @@ var nonOptional: String
 
 num = nil
 str = nil
-nonOptional = nil
 
 // Basic
-var value1 :String?
+var value1: String?
 
 value1 = "こんにちは"
-
-print(value1)
 
 if let value1 = value1{
     print(value1)
@@ -41,8 +38,6 @@ if let unwrappedvalue = value3{
  3. Optinal Chaining(オプショナルチェイング)
  */
 
-
-
 // 1. Forced Unwrapping
 // オプショナル型を強制的にアンラップする方法
 
@@ -57,23 +52,23 @@ print(optional!)
 
 var a: Int = 10  // 非オプショナル型
 var b: Int? = 10 // オプショナル型
-a + b;
+//a + b;
 // => エラーが起きる
 // 非オプショナル型とオプショナル型の計算はできない
 
-var a: Int = 10  // 非オプショナル型
-var b: Int? = 10// オプショナル型
-a + b!; // => 20
+var c: Int = 10  // 非オプショナル型
+var d: Int? = 10// オプショナル型
+print(c + d!) // => 20
 
 
 // 2. Optional Binding
 // if文などの条件式を組み合わせて使っている
 //　アンラップする対象のオプショナル型の変数がnilかどうかを判定するためのもの
 
-var hobby: String? = "swift"
+var language: String? = "swift"
 
-if let unwrappedHobby = hobby{
-    print(unwrappedHobby)
+if let unwrappedLanguage = language {
+    print(unwrappedLanguage)
 } else{
     print("no hobby")
 }
@@ -89,27 +84,42 @@ if let unwrappedHobby = hobby{
 // 変数をアンラップし、中身を取り出すことができる
 //　注意点！
     //　オプショナルチェイニングは続けて所得したプロパティやメソッドの戻り値はオプショナル型になること
-    //  オプショナルチェイニングを使って所得した値は、すべてオプショナル型となるので、その値を使うには再度アンラップが必要    
+    //  オプショナルチェイニングを使って所得した値は、すべてオプショナル型となるので、その値を使うには再度アンラップが必要
 
-//　オプショナルチェイニング（オプショナル型の変数がnilでない場合）
-var human: Human? = Human()
+struct Address {
+    var street: String?
+    var postalCode: String?
+}
 
-human?.name
-// => optional(しんぼ）
-human?.hello()
-// => Optional(こんにちは）
+struct Name {
+    var firstName: String?
+    var lastName: String?
+}
 
+struct Info {
+    var name: Name?
+    var address: Address?
+}
 
-// オプショナルチェイニング(オプショナル型の変数がnilの場合）
-var human: Human?
+class SomeClass {
+    var info: Info?
+    
+    init(info: Info?) {
+        self.info = info
+    }
+}
 
-// humanがnilのため、nilが返る
-human?.name // => nil
+let name = Name(firstName:"Steve", lastName:"Jobs")
+let address = Address(street:"1 Infinite Loop", postalCode:nil)
+var aPerson = SomeClass(info: Info(name:name, address:address))
 
-// humanがnilのため、nilが返る
-human?.hello() // => nil
+// オプショナルチェイニングで取得したので、オプショナルが取得される
+print(aPerson.info?.name?.firstName) // => Optional("Steve")
 
-
-
+if let firstName = aPerson.info?.name?.firstName {
+    print("first name is \(firstName)")
+} else {
+    print("No first name")
+}
 
 
